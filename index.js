e2bbfdf75a8c11b0a652829b0c2a3d0035fb1204
Rikamo <rik.amos.krajinovic@gmail.com>
2022-04-27 19:52:25 +0200

    Made new Event handler and Command handler

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
-// Require the necessary discord.js classes
-// eslint-disable-next-line no-unused-vars
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
 const { token } = require('./config.json');
+const fs = require('node:fs');
 
-// Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
-});
 
-// eslint-disable-next-line no-shadow
-client.on("interactionCreate", async (interaction) => {
+// Fetch and handle events
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+
+// Fetch commands
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+
+// Command handeler
+client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
-	const { commandName } = interaction;
 
-	if (commandName === "ping") {
-		await interaction.reply("pong!");
-	}
-	else if (commandName === "server") {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nServer Creation date: ${interaction.guild.createdAt}\nVerification level: ${interaction.guild.verificationLevel}`);
-	}
-	else if (commandName === "user") {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
-// Login to Discord with your client's token
 client.login(token);
